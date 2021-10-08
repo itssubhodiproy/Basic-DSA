4c59e84d41649504ddc9ec3ddf942b18829173f9
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-08 18:46:40 +0530

    Add files via upload

---
+++ BalancedParenthesis.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool BalancedParenthesis(string s){
+stack<char>st;
+for (int i = 0; i < s.length(); i++)
+{
+    if (s[i]=='('||s[i]=='{'||s[i]=='[')
+    {
+       st.push(s[i]);
+    }
+
+    if (s[i]==')'||s[i]=='}'||s[i]==']')
+    {
+
+    if(st.empty()) return false;             // <<----Edge case(1) 
+       
+       if (s[i]==')' && st.top()=='(' || s[i]=='}' && st.top()=='{'||s[i]==']' && st.top()=='[')
+       {
+           st.pop();
+       } 
+       else
+       {
+           return false;               // If it doesn't match with Top 
+       }
+    }
+}
+if (st.empty())                  
+{
+   return true;
+}
+return false;                           //  <<------Edge case(2)
+
+}
+
+int main(){
+string s="({[]})";
+if (BalancedParenthesis(s))
+{
+    cout<<"Yes! This is a valid parenthesis"<<endl;
+}
+else{
+    cout<<"Nope! This is not a valid parenthesis"<<endl;
+}
+return 0;
+}
