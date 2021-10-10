00977216f467641180aa7f488ff5996e0bd59ddf
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-10 10:40:15 +0530

    Add files via upload

---
+++ LargestRectangleHisBrute.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+    int n;cin>>n;int heights[n];
+    int totalmax=0;int leftmax=0;int rightmax=0;int area=0;
+
+    for(int i=0;i<n;i++){
+        cin>>heights[i];
+    }
+   
+    int maxArea = 0;
+        // int length = sizeof(heights)/sizeof(heights[0]);
+        for (int i = 0; i < n; i++) {
+            int minHeight = INT64_MAX;
+            for (int j = i; j < n ;j++) {
+                minHeight =min(minHeight, heights[j]);
+                maxArea = max(maxArea, minHeight * (j - i + 1));
+            }
+        }
+    cout<<maxArea<<endl;
+    return 0;
+}
